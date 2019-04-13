39016a848d538982da3b765d3eb495910cdcc60f
ChristianMacDonald <chrismacd412@gmail.com>
2019-04-12 23:49:17 -0400

    Added some event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+let nav_links = document.querySelectorAll(".nav-link");
+nav_links.forEach(element => {
+    element.addEventListener("mouseover", function (event) {
+        this["style"]["color"] = "lightgray";
+    });
+    element.addEventListener("mouseout", function (event) {
+        this["style"]["color"] = "";
+    });
+})
+
+let logo_headings = document.querySelector(".logo-heading");
+logo_heading.addEventListener("click", function (event) {
+    this["style"]["color"] = "red";
+});
